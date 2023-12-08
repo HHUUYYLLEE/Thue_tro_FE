788d06e33fc43b8d962d24eb8ed7253688ff3bc4
viethaihust <vuviethaihust@gmail.com>
2023-12-08 15:46:35 +0700

    banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,50 +7,52 @@ import Room4 from '../../asset/img/room4.png'
 export default function Banner() {
   return (
     <div>
-      <img src={BannerImage} alt='' className='absolute -z-1 pointer-events-none' />
-      <div className='z-50 pl-28 pt-24 absolute text-white text-6xl italic'>
-        Tìm phòng trọ uy tín,<br></br> giá rẻ
-      </div>
-      <form className='top-1/3 left-1/2 w-1/2 absolute -translate-x-1/2 -translate-y-1/2'>
-        <div className='relative'>
-          <div className='flex absolute inset-y-0 left-0 items-center pl-8 pointer-events-none z-50'>
-            <svg
-              aria-hidden='true'
-              className='w-5 h-5 text-white'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-              ></path>
-            </svg>
-          </div>
-          <input
-            type='search'
-            id='default-search'
-            className='h-[60px] backdrop-blur placeholder-white bg-white/30 text-white block p-4 pl-16 w-full text-sm border border-gray-300 rounded-[10px]'
-            placeholder='Tìm kiếm quận, huyện, vị trí, phòng trọ,...'
-          ></input>
-          <button
-            type='submit'
-            className='bg-white absolute right-2.5 bottom-[10px] text-blue-700 focus:outline-none italic px-10 py-2 rounded-[20px]'
-          >
-            Tìm kiếm
-          </button>
+      <img src={BannerImage} alt='' className='pointer-events-none' />
+      <div>
+        <div className='left-52 top-32 absolute text-white text-6xl italic'>
+          Tìm phòng trọ uy tín,<br></br> giá rẻ
         </div>
-      </form>
-      <div className='pl-28 pt-[540px]'>
-        <div className='z-50 absolute text-white text-3xl italic'>Các phòng trọ nổi bật</div>
-        <div className='flex absolute gap-4 mt-20'>
-          <img src={Room1} alt=""/>
-          <img src={Room2} alt=""/>
-          <img src={Room3} alt=""/>
-          <img src={Room4} alt=""/>
+        <form className='top-1/3 left-1/2 w-1/2 absolute -translate-x-1/2 -translate-y-1/2'>
+          <div className='relative'>
+            <div className='flex absolute inset-y-0 left-0 items-center pl-8 pointer-events-none'>
+              <svg
+                aria-hidden='true'
+                className='w-5 h-5 text-white'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                ></path>
+              </svg>
+            </div>
+            <input
+              type='search'
+              id='default-search'
+              className='h-[60px] backdrop-blur placeholder-white bg-white/30 text-white block p-4 pl-16 w-full text-sm border border-gray-300 rounded-[10px]'
+              placeholder='Tìm kiếm quận, huyện, vị trí, phòng trọ,...'
+            ></input>
+            <button
+              type='submit'
+              className='bg-white absolute right-2.5 bottom-[10px] text-blue-700 focus:outline-none italic px-10 py-2 rounded-[20px]'
+            >
+              Tìm kiếm
+            </button>
+          </div>
+        </form>
+        <div className='absolute left-52 -mt-60'>
+          <div className='text-white text-3xl italic'>Các phòng trọ nổi bật</div>
+          <div className='flex gap-8 mt-12'>
+            <img src={Room1} alt="" className='max-w-[18vw]'/>
+            <img src={Room2} alt="" className='max-w-[18vw]'/>
+            <img src={Room3} alt="" className='max-w-[18vw]'/>
+            <img src={Room4} alt="" className='max-w-[18vw]'/>
+          </div>
         </div>
       </div>
     </div>
