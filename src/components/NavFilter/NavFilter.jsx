a440a22ed619cf2fe44f56473ae4f60f1461bcce
DucPhamH <ducphamhong2@gmail.com>
2023-12-11 10:36:33 +0700

    fix css

--- src/components/NavFilter/NavFilter.jsx
+++ src/components/NavFilter/NavFilter.jsx
@@ -23,17 +23,17 @@ export default function NavFilter() {
   }, [])
 
   return (
-    <div className='w-full flex justify-center items-center border border-slate-400  px-10 pb-6 pt-8 mb-8'>
+    <div className='w-full flex justify-center items-center border border-slate-400 px-16 pb-6 pt-8 mb-8'>
       <div className='w-5/6 flex justify-between'>
-        <div className='font-bold text-3xl'>Bộ lọc tìm kiếm</div>
-        <div ref={refAddress} className='relative text-lg font-medium py-1 px-3'>
+        <div className='font-bold text-2xl'>Bộ lọc tìm kiếm</div>
+        <div ref={refAddress} className='relative text-lg font-medium py-1'>
           <div
             onClick={openAddressMenu}
             className='flex  hover:text-blue-400 justify-between items-center cursor-pointer border border-black min-w-[18rem] rounded-lg  py-2 px-3'
           >
             <div></div>
             <div className=''>
-              <p className='line-clamp-1 max-w-[10rem]'>{valueAddress == '' ? 'Vị trí' : valueAddress}</p>
+              <p className='line-clamp-1 max-w-[8rem]'>{valueAddress == '' ? 'Vị trí' : valueAddress}</p>
             </div>
             <div>
               <AiOutlineDown />
@@ -42,19 +42,19 @@ export default function NavFilter() {
           {addressMenu && <PopUpAddress setAddressMenu={setAddressMenu} />}
         </div>
 
-        <div className='relative text-lg font-medium py-1 px-3'>
+        <div className='relative text-lg font-medium py-1'>
           <div className='flex text-lg font-medium hover:text-blue-400 justify-between items-center border cursor-pointer border-black min-w-[18rem] rounded-lg py-2 px-3'>
             <div></div>
 
-            <p className='line-clamp-1 max-w-[10rem]'>Loại phòng</p>
+            <p className='line-clamp-1 max-w-[8rem]'>Loại phòng</p>
 
             <div>
               <AiOutlineDown />
             </div>
           </div>
         </div>
-        <div className='relative text-lg font-medium py-1 px-3'>
-          <button className='flex items-center bg-[#0153F2] min-w-[18rem] text-white cursor-pointer hover:bg-sky-600 font-semibold rounded-lg px-5 py-2 mr-3'>
+        <div className='relative text-lg font-medium py-1'>
+          <button className='flex items-center bg-[#0153F2] min-w-[16rem] text-white cursor-pointer hover:bg-sky-600 font-semibold rounded-lg px-5 py-2 mr-3'>
             <div className='flex text-lg justify-center items-center w-full'>
               <div className='mr-3'>Tìm kiếm theo bộ lọc</div>
               <div className='text-lg'>
