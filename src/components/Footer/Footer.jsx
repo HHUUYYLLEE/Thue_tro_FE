fe00fe87a9d5a36624c1165b527eaadc9df2e3d4
HHUUYYLLEE <lebahuybk@gmail.com>
2023-12-11 21:15:14 +0700

    font chữ footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,55 +11,76 @@ export default function Footer() {
       <div className='w-full h-full flex flex-col items-center z-0 bg-[#2D3E52] relative'>
         <img className='w-full' src={footer} />
         <div className='div.row flex w-10/12 justify-center text-white'>
-          <div className='div.col-lg-3 w-1/4'>
-            <div className='div.footer-box'>
-              <h4 className='heading4 font-semibold text-2xl '>
+          <div className='w-1/4'>
+            <div>
+              <h4 className='font-poppins-600 text-2xl '>
                 Về chúng tôi
-                <hr className='w-1/3 border-2 border-white mt-3'></hr>
+                <hr className='w-[5vw] border-2 border-white mt-3'></hr>
               </h4>
 
-              <div className='p.text-custom-white w-3/4 mt-3'>
-                Được phát triển bởi team phát triển thuộc DHBKHN.
+              <div className='font-roboto text-sm w-[13vw] leading-7 mt-3'>
+                Được phát triển bởi team phát triển thuộc DHBKHN
                 <br />
                 Tên nhóm: Dazai
               </div>
               <br />
 
-              <div className='p.text-custom-white w-3/4 '>Website tìm phòng trọ tốt nhất Việt Nam</div>
+              <div className='font-roboto text-sm w-[13vw] leading-7'>Website tìm phòng trọ tốt nhất Việt Nam</div>
 
-              <div className='list w-3/4 mt-3 flex'>
-                <img className='h-7 w-7 mr-3' src={facebook} />
-                <img className='h-7 w-7 mr-3' src={twitter} />
-                <img className='h-7 w-7 mr-3' src={linked} />
-                <img className='h-7 w-7 mr-3' src={youtube} />
+              <div className='list w-[13vw] mt-3 flex justify-between'>
+                <img className='h-9 w-9 mr-3' src={facebook} />
+                <img className='h-9 w-9 mr-3' src={twitter} />
+                <img className='h-9 w-9 mr-3' src={linked} />
+                <img className='h-9 w-9 mr-3' src={youtube} />
               </div>
             </div>
           </div>
 
-          <div className='div.col-lg-3 w-1/4'>
-            <div className='div.footer-box'>
-              <h4 className='heading4 font-semibold text-2xl'>
+          <div className='w-1/4'>
+            <div>
+              <h4 className='font-poppins-600 text-2xl'>
                 Từ khóa
-                <hr className='w-1/3 border-2 border-white mt-3' />
+                <hr className='w-[5vw] border-2 border-white mt-3' />
               </h4>
 
-              <div className='list mt-3'>
-                <div className='item flex'>__ Home</div>
-                <div className='item'>__ Phòng trọ</div>
-                <div className='item'>__ Nhà</div>
-                <div className='item'>__ Chung cư mini</div>
-                <div className='item'>__ Phòng</div>
-                <div className='item'>__ 404</div>
-                <div className='item'>__ Contact Us</div>
+              <div className='list mt-3 font-roboto'>
+                <div className='item'>
+                  <span className='w-[1vw] border-t-[3px] mb-[0.3vh] inline-block bg-white'></span>
+                  <span className='text-sm'> Home</span>
+                </div>
+                <div className='item'>
+                  <span className='w-[1vw] border-t-[3px] mb-[0.3vh] inline-block bg-white'></span>
+                  <span className='text-sm'> Phòng trọ</span>
+                </div>
+                <div className='item'>
+                  <span className='w-[1vw] border-t-[3px] mb-[0.3vh] inline-block bg-white'></span>
+                  <span className='text-sm'> Nhà</span>
+                </div>
+                <div className='item'>
+                  <span className='w-[1vw] border-t-[3px] mb-[0.3vh] inline-block bg-white'></span>
+                  <span className='text-sm'> Chung cư mini</span>
+                </div>
+                <div className='item'>
+                  <span className='w-[1vw] border-t-[3px] mb-[0.3vh] inline-block bg-white'></span>
+                  <span className='text-sm'> Phòng</span>
+                </div>
+                <div className='item'>
+                  <span className='w-[1vw] border-t-[3px] mb-[0.3vh] inline-block bg-white'></span>
+                  <span className='text-sm'> 404</span>
+                </div>
+                <div className='item'>
+                  <span className='w-[1vw] border-t-[3px] mb-[0.3vh] inline-block bg-white'></span>
+                  <span className='text-sm'> Contact Us</span>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className='div.col-lg-3 w-1/4'>
-            <div className='div.footer-box'>
-              <h4 className='heading4 font-semibold text-2xl'>
+          <div className='w-1/4'>
+            <div>
+              <h4 className='font-poppins-600 text-2xl'>
                 Instagram
-                <hr className='w-1/3 border-2 border-white mt-3' />
+                <hr className='w-[5.7vw] border-2 border-white mt-3' />
               </h4>
 
               <div className='list overflow-hidden mt-3'>
@@ -68,26 +89,28 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className='div.col-lg-3 w-1/4'>
-            <div className='div.footer-box'>
-              <h4 className='heading4 font-semibold text-2xl'>
+          <div className='mt-[2vh] w-[25vw]'>
+            <div>
+              <h4 className='font-poppins-600 text-2xl'>
                 Gửi thông tin cho chúng tôi
-                <hr className='w-1/3 border-2 border-white mt-3' />
+                <hr className='w-[5vw] border-2 border-white mt-3' />
               </h4>
 
-              <div className='div.newsletter'>
-                <div className='p.text-custom-white mt-3'>
-                  Hãy gửi nhận xét về trang web của chúng tôi qua email để cải thiện trải nghiệm của bạn
+              <div>
+                <div className='mt-3 max-w-[20vw] text-sm leading-7 font-roboto'>
+                  Hãy gửi nhận xét về trang web của chúng <br /> tôi qua email để cải thiện trải nghiệm của bạn
                 </div>
 
                 <div className='mt-3'>
                   <input
-                    className='shadow appearance-none border rounded-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full'
+                    className='shadow font-roboto appearance-none border rounded-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full'
                     type='text'
                     placeholder='Email Id'
                   />
                   <br />
-                  <button className='bg-white  text-[#01B7F2]  py-2 px-4 rounded-full mt-3'>Đăng ký</button>
+                  <button className='bg-white  text-[#01B7F2] font-roboto py-[2vh] px-[3vw] rounded-full mt-3'>
+                    ĐĂNG KÝ
+                  </button>
                 </div>
               </div>
             </div>
