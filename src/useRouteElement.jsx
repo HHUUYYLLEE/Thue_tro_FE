1f45fc57d0ec207de6cb1722dd32941b6789b8c9
viethaihust <vuviethaihust@gmail.com>
2023-12-17 12:00:02 +0700

    admin

--- src/useRouteElement.jsx
+++ src/useRouteElement.jsx
@@ -5,12 +5,9 @@ import Home from './pages/Home'
 import Bejeweled from './pages/Bejeweled'
 import Error404NotFound from './pages/Error404NotFound/Error404NotFound'
 import RoomProfile from './pages/RoomProfile'
-<<<<<<< HEAD
 import Admin from './pages/Admin'
-=======
 import AdminLayout from './layouts/AdminLayout'
 import AdminDashboard from './pages/AdminDashboard'
->>>>>>> 1bc4b9d8c39d7c49f94c27af33202a520a4abb8c
 
 export default function useRouteElement() {
   const routeElement = useRoutes([
