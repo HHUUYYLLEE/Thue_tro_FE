4040d7a8ed19d1fab538ef55ed46c200c73868e1
DucPhamH <ducphamhong2@gmail.com>
2023-12-22 19:37:00 +0700

    set up api auth

--- src/contexts/app.context.jsx
+++ src/contexts/app.context.jsx
@@ -1,11 +1,16 @@
 import { createContext, useState } from 'react'
+import { getAccessTokenFromLS, getInfoFromLS } from '../utils/auth'
 // import { getAccessTokenFromLS, getInfoFromLS } from '../utils/auth'
 
 const initialAppContext = {
   valueAddress: 'Vị trí',
   setValueAddress: () => null,
   valueQuery: {},
-  setValueQuery: () => null
+  setValueQuery: () => null,
+  isAuthenticated: Boolean(getAccessTokenFromLS()),
+  setIsAuthenticated: () => null,
+  info: getInfoFromLS(),
+  setInfo: () => null
 }
 
 export const AppContext = createContext(initialAppContext)
@@ -13,13 +18,19 @@ export const AppContext = createContext(initialAppContext)
 export const AppProvider = ({ children }) => {
   const [valueAddress, setValueAddress] = useState(initialAppContext.valueAddress)
   const [valueQuery, setValueQuery] = useState(initialAppContext.valueQuery)
+  const [isAuthenticated, setIsAuthenticated] = useState(initialAppContext.isAuthenticated)
+  const [info, setInfo] = useState(initialAppContext.info)
   return (
     <AppContext.Provider
       value={{
         valueAddress,
         setValueAddress,
         valueQuery,
-        setValueQuery
+        setValueQuery,
+        isAuthenticated,
+        setIsAuthenticated,
+        info,
+        setInfo
       }}
     >
       {children}
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-// import { toast } from 'react-toastify'
-// import HttpStatusCode from '../constants/httpStatusCode.enum'
-// import { clearAccessTokenFromLS, getAccessTokenFromLS, saveAccessTokenToLS, saveInfoFromLS } from './auth'
+import { toast } from 'react-toastify'
+import HttpStatusCode from '../constants/httpStatusCode.enum'
+import { clearAccessTokenFromLS, getAccessTokenFromLS, saveAccessTokenToLS, saveInfoFromLS } from './auth'
 
 const URL = {
   BASE_URL: 'http://localhost:4000/api/v1/',
@@ -16,63 +16,60 @@ const instance = axios.create({
   }
 })
 
-// var accessToken = getAccessTokenFromLS()
+var accessToken = getAccessTokenFromLS()
 
-// instance.interceptors.request.use(
-//   (config) => {
-//     //console.log(config)
-//     if (accessToken && config.headers) {
-//       config.headers.Authorization = accessToken
-//       //console.log(config.headers.Authorization)
-//       return config
-//     }
-//     return config
-//   },
-//   function (error) {
-//     // Do something with request error
-//     return Promise.reject(error)
-//   }
-// )
-// instance.interceptors.response.use(
-//   (response) => {
-//     // console.log(response)
-//     const { url } = response.config
+instance.interceptors.request.use(
+  (config) => {
+    //console.log(config)
+    if (accessToken && config.headers) {
+      config.headers.Authorization = accessToken
+      //console.log(config.headers.Authorization)
+      return config
+    }
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+instance.interceptors.response.use(
+  (response) => {
+    // console.log(response)
+    const { url } = response.config
 
-//     if (url === 'users/login') {
-//       // console.log(url)
+    if (url === 'host/login') {
+      // console.log(url)
 
-//       accessToken = response.data.data.accessToken
-//       //console.log(accessToken)
-//       saveInfoFromLS(response.data.data.user)
-//       saveAccessTokenToLS(accessToken)
-//     } else if (url === 'users/logout') {
-//       console.log(response.data)
-//       accessToken = ''
-//       clearAccessTokenFromLS()
-//       // toast(response.data.message)
-//       toast('ログアウト成功')
-
-//       // window.location.reload()
-//     }
-//     return response
-//   },
-//   function (error) {
-//     if (error.response?.status !== HttpStatusCode.BadRequest) {
-//       const data = error.response?.data
-//       const message = data.message || error.message
-//       toast.error(message)
-//     }
-//     if (error.response?.status === HttpStatusCode.Unauthorized) {
-//       const data = error.response?.data
-//       console.log(data)
-//       const message = data.message || error.message
-//       clearAccessTokenFromLS()
-//       toast.error(message)
-//       window.location.reload()
-//     }
-//     return Promise.reject(error)
-//   }
-// )
+      accessToken = response.data.data.accessToken
+      //console.log(accessToken)
+      saveInfoFromLS(response.data.data.user)
+      saveAccessTokenToLS(accessToken)
+    } else if (url === 'host/logout') {
+      console.log(response.data)
+      accessToken = ''
+      clearAccessTokenFromLS()
+      toast('Đăng xuất thành công')
+    }
+    return response
+  },
+  function (error) {
+    if (error.response?.status !== HttpStatusCode.BadRequest) {
+      const data = error.response?.data
+      const message = data.message || error.message
+      toast.error(message)
+    }
+    if (error.response?.status === HttpStatusCode.Unauthorized) {
+      const data = error.response?.data
+      console.log(data)
+      const message = data.message || error.message
+      clearAccessTokenFromLS()
+      toast.error(message)
+      window.location.reload()
+    }
+    return Promise.reject(error)
+  }
+)
 
 const http = instance
 
