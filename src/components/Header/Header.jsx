c2738316e9dc46c6268e76f969dba27cf31029b2
viethaihust <vuviethaihust@gmail.com>
2023-12-22 22:07:05 +0700

    avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -52,7 +52,7 @@ export default function Header() {
             tro.vn
           </Link>
         </div>
-        <div className='flex ml-60 gap-8 font-poppins-500'>
+        <div className='flex ml-80 gap-8 font-poppins-500'>
           {/* {headerItems.map((item) => {
           return (
             <NavLink
@@ -81,6 +81,8 @@ export default function Header() {
             to='/'
             className={`group text-lg transition duration-300 ${
               path.pathname.includes('/room') ? 'text-black' : 'text-white'
+            } ${
+              isAuthenticated && 'ml-28'
             }`}
           >
             Trang chủ
@@ -127,54 +129,51 @@ export default function Header() {
               }`}
             />
           </NavLink> */}
-          {!isAuthenticated ? (
-            <>
-              <div
-                onClick={openModalLogin}
-                className={`group cursor-pointer text-lg transition duration-300 ${
-                  path.pathname.includes('/room') ? 'text-black' : 'text-white'
+          <div className={`flex flex-row gap-8 ${isAuthenticated && 'invisible'}`}>
+            <div
+              onClick={openModalLogin}
+              className={`group cursor-pointer text-lg transition duration-300 ${
+                path.pathname.includes('/room') ? 'text-black' : 'text-white'
+              }`}
+            >
+              Đăng nhập
+              <span
+                className={`${
+                  path.pathname === '/login'
+                    ? 'max-w-[80%]'
+                    : 'max-w-0 group-hover:max-w-[80%] transition-all duration-500'
+                } block rounded-xl mt-[0.5vh] h-[0.1rem] mx-auto ${
+                  path.pathname.includes('/room') ? 'bg-black' : 'bg-white'
                 }`}
-              >
-                Đăng nhập
-                <span
-                  className={`${
-                    path.pathname === '/login'
-                      ? 'max-w-[80%]'
-                      : 'max-w-0 group-hover:max-w-[80%] transition-all duration-500'
-                  } block rounded-xl mt-[0.5vh] h-[0.1rem] mx-auto ${
-                    path.pathname.includes('/room') ? 'bg-black' : 'bg-white'
-                  }`}
-                />
-              </div>
-              <NavLink
-                onClick={() => this.forceUpdate}
-                to='/signup'
-                className={`group text-lg transition duration-300 ${
-                  path.pathname.includes('/room') ? 'text-black' : 'text-white'
-                }`}
-              >
-                Đăng ký
-                <span
-                  className={`${
-                    path.pathname === '/signup'
-                      ? 'max-w-[80%]'
-                      : 'max-w-0 group-hover:max-w-[80%] transition-all duration-500'
-                  } block rounded-xl mt-[0.5vh] h-[0.1rem] mx-auto ${
-                    path.pathname.includes('/room') ? 'bg-black' : 'bg-white'
-                  }`}
-                />
-              </NavLink>
-            </>
-          ) : (
-            <div className='flex mr-20 items-center gap-2 cursor-pointer'>
-              <div className='bg-gray-300 rounded-full w-[3rem] h-[3rem] flex items-center justify-center'>
-                <img src={Pepega} alt='' className='w-[2rem] h-[2rem]' />
-              </div>
-              <div className={`font-semibold ${path.pathname.includes('/room') ? 'text-black' : 'text-white'}`}>
-                {info.user_name}
-              </div>
+              />
             </div>
-          )}
+            <NavLink
+              onClick={() => this.forceUpdate}
+              to='/signup'
+              className={`group text-lg transition duration-300 ${
+                path.pathname.includes('/room') ? 'text-black' : 'text-white'
+              }`}
+            >
+              Đăng ký
+              <span
+                className={`${
+                  path.pathname === '/signup'
+                    ? 'max-w-[80%]'
+                    : 'max-w-0 group-hover:max-w-[80%] transition-all duration-500'
+                } block rounded-xl mt-[0.5vh] h-[0.1rem] mx-auto ${
+                  path.pathname.includes('/room') ? 'bg-black' : 'bg-white'
+                }`}
+              />
+            </NavLink>
+          </div>
+        </div>
+        <div className={`flex mr-20 items-center gap-2 cursor-pointer ${!isAuthenticated && 'invisible'}`}>
+          <div className='bg-gray-300 rounded-full w-[3rem] h-[3rem] flex items-center justify-center'>
+            <img src={Pepega} alt='' className='w-[2rem] h-[2rem]' />
+          </div>
+          <div className={`font-semibold ${path.pathname.includes('/room') ? 'text-black' : 'text-white'}`}>
+            {info?.user_name}
+          </div>
         </div>
       </header>
       {modalLogin && <LoginModal closeModalLogin={closeModalLogin} />}
