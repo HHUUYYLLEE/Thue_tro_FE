da2bd50215bdb96da5cbf19e908d42abd79b8157
DucPhamH <ducphamhong2@gmail.com>
2023-12-22 19:59:31 +0700

    set up form login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
+import LoginModal from '../LoginModal'
 const headerItems = [
   { id: 1, name: 'Trang chủ', path: '/' },
   { id: 2, name: 'Đăng nhập', path: '/login' },
@@ -8,6 +9,14 @@ const headerItems = [
 
 export default function Header() {
   const [header, setHeader] = useState(false)
+  const [modalLogin, setModalLogin] = useState(false)
+
+  const openModalLogin = () => {
+    setModalLogin(true)
+  }
+  const closeModalLogin = () => {
+    setModalLogin(false)
+  }
   const changeBackground = () => {
     if (window.scrollY > 100) {
       setHeader(true)
@@ -23,24 +32,25 @@ export default function Header() {
   window.addEventListener('scroll', changeBackground)
 
   return (
-    <header
-      className={` fixed flex top-0 w-full h-20 backdrop-filter shadow-lg justify-between items-center transition duration-300 z-[999] ${
-        path.pathname.includes('/room') === true
-          ? 'bg-white'
-          : `${header === true ? 'bg-slate-900/70' : 'bg-transparent'}`
-      }`}
-    >
-      <div
-        className={`pl-20 -mt-2 text-3xl italic ${
-          path.pathname.includes('/room') === true ? 'text-blue-500' : 'text-white'
+    <>
+      <header
+        className={` fixed flex top-0 w-full h-20 backdrop-filter shadow-lg justify-between items-center transition duration-300 z-[990] ${
+          path.pathname.includes('/room') === true
+            ? 'bg-white'
+            : `${header === true ? 'bg-slate-900/70' : 'bg-transparent'}`
         }`}
       >
-        <Link onClick={() => this.forceUpdate} to='/'>
-          tro.vn
-        </Link>
-      </div>
-      <div className='flex pl-60 gap-8 font-poppins-500'>
-        {headerItems.map((item) => {
+        <div
+          className={`pl-20 -mt-2 text-3xl italic ${
+            path.pathname.includes('/room') === true ? 'text-blue-500' : 'text-white'
+          }`}
+        >
+          <Link onClick={() => this.forceUpdate} to='/'>
+            tro.vn
+          </Link>
+        </div>
+        <div className='flex pl-60 gap-8 font-poppins-500'>
+          {/* {headerItems.map((item) => {
           return (
             <NavLink
               onClick={() => this.forceUpdate}
@@ -62,9 +72,62 @@ export default function Header() {
               />
             </NavLink>
           )
-        })}
-      </div>
-      <div></div>
-    </header>
+        })} */}
+          <NavLink
+            onClick={() => this.forceUpdate}
+            to='/'
+            className={`group text-lg transition duration-300 ${
+              path.pathname.includes('/room') ? 'text-black' : 'text-white'
+            }`}
+          >
+            Trang chủ
+            <span
+              className={`${
+                path.pathname === '/' ? 'max-w-[80%]' : 'max-w-0 group-hover:max-w-[80%] transition-all duration-500'
+              } block rounded-xl mt-[0.5vh] h-[0.1rem] mx-auto ${
+                path.pathname.includes('/room') ? 'bg-black' : 'bg-white'
+              }`}
+            />
+          </NavLink>
+          <div
+            onClick={openModalLogin}
+            className={`group cursor-pointer text-lg transition duration-300 ${
+              path.pathname.includes('/room') ? 'text-black' : 'text-white'
+            }`}
+          >
+            Đăng nhập
+            <span
+              className={`${
+                path.pathname === '/login'
+                  ? 'max-w-[80%]'
+                  : 'max-w-0 group-hover:max-w-[80%] transition-all duration-500'
+              } block rounded-xl mt-[0.5vh] h-[0.1rem] mx-auto ${
+                path.pathname.includes('/room') ? 'bg-black' : 'bg-white'
+              }`}
+            />
+          </div>
+          <NavLink
+            onClick={() => this.forceUpdate}
+            to='/signup'
+            className={`group text-lg transition duration-300 ${
+              path.pathname.includes('/room') ? 'text-black' : 'text-white'
+            }`}
+          >
+            Đăng ký
+            <span
+              className={`${
+                path.pathname === '/signup'
+                  ? 'max-w-[80%]'
+                  : 'max-w-0 group-hover:max-w-[80%] transition-all duration-500'
+              } block rounded-xl mt-[0.5vh] h-[0.1rem] mx-auto ${
+                path.pathname.includes('/room') ? 'bg-black' : 'bg-white'
+              }`}
+            />
+          </NavLink>
+        </div>
+        <div></div>
+      </header>
+      {modalLogin && <LoginModal closeModalLogin={closeModalLogin} />}
+    </>
   )
 }
--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,5 +1,52 @@
 import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+export default function LoginModal({ closeModalLogin }) {
+  return (
+    <div className='modal '>
+      <div className='overlay' onClick={() => closeModalLogin()}></div>
+      <div className='modal-content min-w-[360px] md:min-w-[400px] bg-white'>
+        <div className='relative w-full max-w-md max-h-full'>
+          {/* <div
+            onClick={() => closeModalLogin()}
+            className='absolute right-0 top-[-0.5rem] rounded-full transition-all duration-300  cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-500 flex justify-center items-center h-8 w-8  dark:text-yellow-400 font-extrabold'
+          >
+            <AiOutlineClose />
+          </div> */}
+          <div className='font-itim'>
+            <div className='flex w-full justify-between items-center pb-10 px-10'></div>
+            <form className='w-full px-10' noValidate>
+              <div className=' relative mb-10'>
+                <label className='block mb-2 text-xl text-black'>Tài khoản</label>
 
-export default function LoginModal() {
-  return <div>LoginModal</div>
+                <input
+                  type='text'
+                  id='phone'
+                  name='phone'
+                  className='bg-[#D9D9D9] border border-gray-300 text-gray-900 text-2xl rounded-full  block w-full p-4'
+                />
+              </div>
+
+              <div className=' relative mb-10'>
+                <label className='block mb-2 text-xl text-black'>Mật khẩu</label>
+                <div className='absolute right-0 flex items-center pr-5 justify-center bottom-12 top-[40%] cursor-pointer scale-150'></div>
+                <input
+                  id='password'
+                  type='password'
+                  name='password'
+                  autoComplete='on'
+                  className='bg-[#D9D9D9] border border-gray-300 text-gray-900 text-2xl rounded-full  block w-full p-4'
+                />
+              </div>
+
+              <div className='w-full flex justify-center items-center pt-14'>
+                <button className='bg-[#D324D7] hover:bg-blue-500  mx-5 text-white py-4 px-8 border rounded-full'>
+                  Đăng nhập
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
